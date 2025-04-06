0d357e1c031f14759eca534d67bbbc8a90bb4a6d
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2025-04-06 07:54:33 -0700

    created linkedlist class

---
+++ module-4/LinkedListTraversalTest.java
@@ -0,0 +1,12 @@
+public class LinkedListTraversalTest {
+    public static void main(String[] args) {
+        // Test with 50,000 integers
+        System.out.println("Testing with 50,000 integers...");
+        testListTraversal(50000);
+
+        // Test with 500,000 integers
+        System.out.println("Testing with 500,000 integers...");
+        testListTraversal(500000);
+    }
+}
+
