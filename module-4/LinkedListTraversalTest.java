f03b014abd05f6bf2427c723dd05ac97d712959b
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2025-04-06 07:55:38 -0700

    Added private static

--- module-4/LinkedListTraversalTest.java
+++ module-4/LinkedListTraversalTest.java
@@ -10,3 +10,41 @@ public class LinkedListTraversalTest {
     }
 }
 
+private static void testListTraversal(int size) {
+    LinkedList<Integer> list = new LinkedList<>();
+
+    // Populate the list with integers from 1 to 'size'
+    for (int i = 1; i <= size; i++) {
+        list.add(i);
+    }
+
+    // Measure time to traverse using Iterator
+    long startTime = System.nanoTime();
+    traverseWithIterator(list);
+    long endTime = System.nanoTime();
+    long iteratorTime = endTime - startTime;
+    System.out.println("Time taken to traverse using iterator: " + iteratorTime + " nanoseconds.");
+
+    // Measure time to traverse using get(index)
+    startTime = System.nanoTime();
+    traverseWithGetMethod(list);
+    endTime = System.nanoTime();
+    long getMethodTime = endTime - startTime;
+    System.out.println("Time taken to traverse using get(index): " + getMethodTime + " nanoseconds.");
+
+    // Calculate and print the difference in time
+    System.out.println("Difference in time: " + (getMethodTime - iteratorTime) + " nanoseconds.");
+}
+
+private static void traverseWithIterator(LinkedList<Integer> list) {
+    Iterator<Integer> iterator = list.iterator();
+    while (iterator.hasNext()) {
+        iterator.next(); // We don't need to do anything with the value
+    }
+}
+
+private static void traverseWithGetMethod(LinkedList<Integer> list) {
+    for (int i = 0; i < list.size(); i++) {
+        list.get(i); // We don't need to do anything with the value
+    }
+}
