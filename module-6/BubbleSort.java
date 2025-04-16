4c6d6437fb4f38ebbd24dba5b7f20a4a0c250578
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2025-04-15 20:57:29 -0700

    added class that uses comparator

--- module-6/BubbleSort.java
+++ module-6/BubbleSort.java
@@ -20,4 +20,22 @@ public class BubbleSort {
         }
     }
 
-    
+    public static <E> void bubbleSort(E[] list, Comparator<? super E> comparator) {
+        int n = list.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (comparator.compare(list[j], list[j + 1]) > 0) {
+                    E temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+}
+
